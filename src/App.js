11a278bc14dfc86b3a6678db336521085f6a4249
GitPatout <patout.julien.02@gmail.com>
2022-05-20 15:51:56 +0200

    Organisation de route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import Navbar from './Navbar/navbar.js';
-import './Navbar/navbar.css';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="app">
-    <Navbar/>
-    </div>
-    
-  );
+ 
 }
 
 export default App;
